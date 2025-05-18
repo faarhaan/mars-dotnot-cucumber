384a76f2b17ef0808e653c9171b9b62e8c9151e0
faarhaan <faarhaan.au@gmail.com>
2025-05-18 08:59:06 +0500

    POM is Implemented

---
+++ mars-dotnetCucumber/Pages/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace mars_dotnetCucumber.Pages
+{
+    public class HomePage
+    {
+        // verify user is login successfully
+        public void userIsInHomePage(IWebDriver driver)
+        {
+
+            IWebElement MarsLogo = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/a"));
+            if (MarsLogo.Text == "Mars Logo")
+            {
+                Console.WriteLine("User Login Successfully! Test is Passed");
+            }
+            else
+            {
+                Console.WriteLine("User didn't Login! Test Failed");
+            }
+
+        }
+    }
+}
---
+++ mars-dotnetCucumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using mars_dotnetCucumber.Pages;
+using mars_dotnetCucumber.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        //  Launch Mars Portal
+        IWebDriver driver = new ChromeDriver();
+        LoginPage loginPageObj = new LoginPage();
+        loginPageObj.LoginActions(driver);
+
+        // Verify user is loged in successfully
+        HomePage homePageObj = new HomePage();
+        homePageObj.userIsInHomePage(driver);
+
+        // Add languages to the list
+        LSPage lSObj = new LSPage();
+        lSObj.languagePage(driver);
+
+        // Add Skills to the List
+        LSPage lSPageObj = new LSPage();
+        lSPageObj.SkillPage(driver);
+
+        /*// Update Last Language
+        LSPage lSPageObj = new LSPage();
+        lSPageObj.UpdateLastRecord(driver);*/
+            
+ 
+        
+
+        
+    }
+
+
+
+
+
+
+}
